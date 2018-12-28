685ab661848b735f1e2c53e579b01f782901ee9e
Jared Ellison <jaredellison@gmail.com>
2018-12-28 12:34:52 -0500

    Save individual service URLs in json configuration file

--- server/index.js
+++ server/index.js
@@ -4,32 +4,51 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
 
+////////////////////////////////////////
+//  Remote Server Endpoints
+const service_urls = require('../service_urls.json');
+
 let app = express();
 
 let PORT = process.env.PORT || 3000;
 
 let staticPath = path.join(__dirname, '../public');
 
-////////////////////////////////////////
-//  Apply Middleware and Serve Static Site
-
+////////////////////
+//  Apply Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+
+/////////////////////
+//  Serve Static Site
 app.use('/:id(\\d+)/', express.static(staticPath));
 
 ////////////////////////////////////////
 //  API Call Redirection
 
 // Carousel
-app.get('/api/carousel/:id', (req, res) => {
-  console.log('req.params:', req.params);
-  res.sendStatus(200);
+app.get(service_urls.carousel.LOCAL_API_TARGET, (req, res) => {
+  const itemId = req.params.id;
+  axios.get(service_urls.carousel.API_URL + itemId)
+  .then((result) => {
+    res.json(result.data);
+  })
+  .catch((err) => {
+    console.log('Error fetching from carousel API:',err);
+    res.sendStatus(500);
+  })
 })
+
 // Details
 
 // Description
 
 // Similar Listings
+
+
+// Catch any other requests
+app.use('/', express.static(staticPath));
 
 ////////////////////////////////////////
 //  Instantate Server
